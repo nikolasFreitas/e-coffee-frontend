6232a878750b6594389a9f2b1b6409c3c11844d0
githiago-f <52945005+githiago-f@users.noreply.github.com>
2020-10-11 22:34:58 +0000

    Deploying to gh-pages from  @ 4149566a4edcbb9d0a0b907232f41b2c75f0fa16 🚀

---
+++ precache-manifest.05e98a106e78044a3ab71e7239abc366.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08d3d4a63fe36ecbb9095f0fc628ddf6",
+    "url": "/e-coffee-frontend/index.html"
+  },
+  {
+    "revision": "44f9ce72dcd4f582e048",
+    "url": "/e-coffee-frontend/static/js/2.9547190f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/e-coffee-frontend/static/js/2.9547190f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cdf053873c8aa33cca2",
+    "url": "/e-coffee-frontend/static/js/main.dc3099d7.chunk.js"
+  },
+  {
+    "revision": "c263309668763cefb193",
+    "url": "/e-coffee-frontend/static/js/runtime-main.e6388521.js"
+  },
+  {
+    "revision": "95a76351cb7fa9668208f3e90855b41f",
+    "url": "/e-coffee-frontend/static/media/coffee.95a76351.svg"
+  }
+]);
